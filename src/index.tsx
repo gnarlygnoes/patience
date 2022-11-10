84c7098b0abe67505e428bf8426745f97ab11531
gnarlygnoes <shpandee@gmail.com>
2022-11-11 12:08:51 +1300

    added some representations of cards

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { Rank } from "./types/types";
 
 function App() {
+  console.log(Rank["ace"]);
+
   return (
     <div>
       <h1>Hello</h1>
---
+++ src/types/types.ts
@@ -0,0 +1,40 @@
+import { Reducer } from "react";
+
+export type Card = {
+  suit: Suit;
+  rank: Rank;
+};
+
+export type Suit =
+  | {
+      name: "hearts";
+      colour: "red";
+    }
+  | {
+      name: "diamonds";
+      colour: "red";
+    }
+  | {
+      name: "clubs";
+      colour: "black";
+    }
+  | {
+      name: "spades";
+      colour: "black";
+    };
+
+export enum Rank {
+  ace,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  jack,
+  queen,
+  king,
+}
