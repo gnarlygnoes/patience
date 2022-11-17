dd9fbffe7df142c3d2a9f3d12356eae27bda7d0b
Toby2 <sugto555@gmail.com>
2022-11-18 11:23:38 +1300

    Draw empty card to the screen in a grid.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { Rank } from "./types/types";
-import { cards } from "./cards";
+import { cards } from "./card-data";
+import { Card } from "./card";
 
 function App() {
   console.log(cards);
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="Cards">
+      {cards.map((card) => (
+        <Card key={card.rank + card.suit.name} />
+      ))}
     </div>
   );
 }
